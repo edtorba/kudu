f6d7cfa4707322db0bc657aeefa8807cf895a9fd
Ed <eduards.torba@gmail.com>
2015-02-24 01:04:00 +0000

    static.js minor changes

--- public/js/static.js
+++ public/js/static.js
@@ -1,21 +1,21 @@
-function toggleClass(elem, className) {
-
-    if (elem.classList) {
-        elem.classList.toggle(className);
+// Toggle class name on DOM element
+function toggleClass(ele, className) {
+    if (ele.classList) {
+        ele.classList.toggle(className);
     } else {
-        var classes = elem.className.split(' ');
+        var classes = ele.className.split(' ');
         var existingIndex = classes.indexOf(className);
 
         if (existingIndex >= 0)
-            classes.splice(existingIndex, 1);
+          classes.splice(existingIndex, 1);
         else
-            classes.push(className);
+          classes.push(className);
 
-        elem.className = classes.join(' ');
+        ele.className = classes.join(' ');
     }
 };
 
-
+// Check if variable is empty
 function isEmpty(text) {
     return (text.length === 0 || !text.trim());
 };
