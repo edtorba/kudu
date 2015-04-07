6f832982185163a0b7ded79e384eafd35ab670ee
Ed <eduards.torba@gmail.com>
2015-04-07 15:15:09 +0100

    Player class refactoring

--- classes/player.js
+++ classes/player.js
@@ -4,25 +4,33 @@
  * Player Class
  */
 function Player() {
-    this.car = null;
     this.score = 0;
     this.money = 0;
-    this.armor = 0;
-    this.speed = 0;
-    this.power = 0;
+    this.car = {
+        'model': null,
+        'specs': {
+            'armor': 0,
+            'speed': 0,
+            'power': 0
+        }
+    };
     this.health = 1000;
     this.lives = 5;
     this.disqualified = false;
+    this.coordinates = {
+        'x': 0,
+        'y': 0
+    };
 };
 
 // Check if player has already selected car
 Player.prototype.hasCar = function() {
-    return this.car != null;
+    return this.car.model != null;
 };
 
 // Assignes a car object to player
 Player.prototype.setCar = function(carObj) {
-    this.car = carObj;
+    this.car.model = carObj;
 };
 
 module.exports = Player;
