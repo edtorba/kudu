4712145acfca52ce0b461b9964efba29881c12d4
Ed <eduards.torba@gmail.com>
2015-04-08 17:03:11 +0100

    Game.js (PC Browsers) rAF

--- public/js/game.js
+++ public/js/game.js
@@ -3,11 +3,12 @@
 function Game() {
     var container = document.querySelector('.js--game-canvas');
     var _self = this;
+    this.rAFId;
 
     // Create canvas element
     this.canvas = createEle(false, 'canvas');
     this.canvas.width = window.innerWidth;
-    this.canas.height = window.innerHeight;
+    this.canvas.height = window.innerHeight;
 
     // Get context
     this.context = this.canvas.getContext('2d');
@@ -18,7 +19,25 @@ function Game() {
 
 Game.prototype.loop = function() {
     var _self = this;
+
+    this.rAFId = window.requestAnimationFrame(_self.loop.bind(this));
     // TODO
+    console.log('Loop is working');
+};
+
+Game.prototype.start = function() {
+    var _self = this;
+    if (!_self.rAFId) {
+        _self.loop();
+    }
+};
+
+Game.prototype.stop = function() {
+    var _self = this;
+    if (_self.rAFId) {
+        window.cancelAnimationFrame(_self.rAFId);
+        _self.rAFId = undefined;
+    }
 };
 
 Game.prototype.drawPlayers = function() {
